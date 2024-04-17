6ebc02e0c0f67cc723dbb0299f75b741c92cf367
Daniił Žarnasiek <63464212+ddjoul@users.noreply.github.com>
2024-04-17 08:36:43 +0300

    Update main.cpp

--- matrix/matrix/main.cpp
+++ matrix/matrix/main.cpp
@@ -9,11 +9,26 @@
 //Жерносек Даниил, 11 группа
 int main() {
     srand(std::time(NULL));
-    SquareMatrix<2> a;
-    SquareMatrix<2> b;
-    SquareMatrix<2> answer;
-    std::cin >> a >> b;
-    a.invert();
-    answer = b * a;
-    std::cout << answer;
+    std::vector<std::vector<Rational>> arr{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    Matrix<3, 4, Rational> m1(arr);
+    Matrix<3, 4> m2(arr);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            m2[i][j] = Rational(rand() % 6);
+        }
+    }
+    std::cout << "m1<3, 4>:\n" << m1 << "rank m1:" << m1.rank();
+    std :: cout << "\nm2<3, 4>\n" << m2 << "rank m2:" << m2.rank() << "\nm1 + m2:\n" << m1 + m2 << "m2.transpored:\n" << m2.transpored();
+
+    Matrix<3, 3> sq_m1;
+    std::vector<std::vector<Rational>> sq_arr{ {1, 2, 3}, {4, 5, 6}, {7, 8, -9} };
+    SquareMatrix<3> sq_m2(sq_arr);
+    std::cout << "Enter elements of square matrix #1:\n";
+    std::cin >> sq_m1;
+    std::cout << "sq_m1<3, 3>:\n" << sq_m1 << "trace sq_m1:" << sq_m1.trace() << "\ndet sq_m1:" << sq_m1.det();
+    std::cout << "\nsq_m2<3>:\n" << sq_m2 << "trace sq_m2:" << sq_m2.trace() << "\ndet sq_m2:" << sq_m2.det();
+    std::cout << "\ninverted:\n" << sq_m2.inverted();
+    std::cout << "\n sq_m2 * m2\n" << sq_m2 * m2;
+    sq_m1 *= sq_m1;
+    std::cout << "\n(sq_m1)^2\n" << sq_m1;
 }
